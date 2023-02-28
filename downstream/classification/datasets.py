c48f9f71e20f92cf123139b22d87bb56324a6b27
Kyungjin Cho / 조경진 <108312461+kyungjincho@users.noreply.github.com>
2023-02-28 11:00:52 +0900

    dataset.py update

--- downstream/classification/datasets.py
+++ downstream/classification/datasets.py
@@ -1,22 +1,20 @@
-import os
-import sys
-import numpy as np
-import torch
-import random
 import glob
+import json
+import os
+import random
+import sys
 
-from torch.utils.data import Dataset
-
+import albumentations as A
+import cv2
+import numpy as np
+import SimpleITK as sitk
+import torch
 import torchvision
-# import torchvision.transforms as T
-
+from albumentations.pytorch import ToTensorV2
 from config import parse_arguments
 from PIL import Image
-import cv2
-import SimpleITK as sitk
-import json
-import albumentations as A
-from albumentations.pytorch import ToTensorV2
+from torch.utils.data import Dataset
+
 
 class DiseaseDataset(Dataset):
     def __init__(self, input_path, mode, image_size, bits, args, transform=None):
@@ -38,36 +36,28 @@ class DiseaseDataset(Dataset):
                     A.OneOf([
                         A.MedianBlur(blur_limit=3, p=0.1),
                         A.MotionBlur(p=0.2),
-                        # A.IAASharpen(p=0.2),
                         ], p=0.2),
                     A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.2, rotate_limit=10, p=0.2),
                     A.OneOf([
                         A.OpticalDistortion(p=0.3),
                         ], p=0.2),
-                    # A.OneOf([
-                    #     A.CLAHE(clip_limit=4.0),
-                    #     A.Equalize(),
-                    #     ], p=0.2),
                     A.OneOf([
                         A.GaussNoise(p=0.2),
                         A.MultiplicativeNoise(p=0.2),
                         ], p=0.2),
                     A.HueSaturationValue(hue_shift_limit=0, sat_shift_limit=0, val_shift_limit=0.1, p=0.3),
-                    # A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
                     A.Normalize(mean=(0.485), std=(0.229)),
                     ToTensorV2(),
                     ])
             else:
                 self.transform = A.Compose([
                     A.Resize(self.image_size, self.image_size),
-                    # A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
                     A.Normalize(mean=(0.485), std=(0.229)),
                     ToTensorV2(),
                     ])
         else:
             self.transform = A.Compose([
                 A.Resize(self.image_size, self.image_size),
-                # A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
                 A.Normalize(mean=(0.485), std=(0.229)),
                 ToTensorV2(),
                 ])
@@ -82,23 +72,23 @@ class DiseaseDataset(Dataset):
         return len(self.samples['labels'])
     
     def _preprocessing(self, path , bits):
-        if bits == 8:
-            img = np.array(Image.open(path))
-            img = self._min_max_scaling(img)
-        else:
-            img = np.load(path)
-        img = self._standardization(img)
+        img = cv2.imread(path)
+        img = self.cutoff(img, bits)
         return img
 
-    def _min_max_scaling(self, img):
-        return (img-np.min(img)) / (np.max(img)-np.min(img))
+    # our  preprocessing
+    def cutoff(self,img, bits):
+        img = cv2.resize(img,(512,512))
+        img = np.clip(img, 0, np.percentile(img, 99))
+        img -= img.min()
+        img /= (img.max() - img.min())
+        img *= 255
+        if bits == 8:
+            img = img.astype(np.uint8)
+        else:
+            img = img.astype(np.float32)
+        return img
     
-    def _standardization(self, img):
-        np_img = img.astype(np.float32)
-        mean, std = np.mean(np_img), np.std(np_img)
-        norm_img = (np_img-mean)/std
-        return norm_img
-
 # For test
 if __name__ == '__main__':
     dataset = DiseaseDataset('./json/dummy.json')
